772281c077af4f6c9f80525d2b217a590f21a391
Bilal Abdullah <abdullahbilal88@gmail.com>
2018-04-06 00:55:26 +0200

    Added JobRequest repository

--- web-scraper/src/main/java/ee/bilal/dev/engine/webscraper/domain/repository/JobRequestRepository.java
+++ web-scraper/src/main/java/ee/bilal/dev/engine/webscraper/domain/repository/JobRequestRepository.java
@@ -1,10 +1,10 @@
 package ee.bilal.dev.engine.webscraper.domain.repository;
 
-import ee.bilal.dev.engine.webscraper.domain.model.ReportJob;
+import ee.bilal.dev.engine.webscraper.domain.model.JobRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JobReportRepository extends JpaRepository<ReportJob, String> {
+public interface JobRequestRepository extends JpaRepository<JobRequest, String> {
 
 }
